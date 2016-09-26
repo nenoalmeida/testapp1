365d74b07f447310b4ad76380b0496911a03dcb3
Neno Almeida <nenoxvx@icloud.com>
2016-09-26 02:40:15 +0000

    Criando features para teste

--- testapp1/spec/Features/create_posts_spec.rb
+++ testapp1/spec/Features/create_posts_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper.rb'
+
+feature 'Creating Posts' do
+    scenario 'can create job' do
+        #visit root rout
+        visit '/' 
+        #create a post link
+        click link 'Creat Post'
+        #fill the form with needed information (title and caption)  
+        Fill_in 'Title', with: 'title'
+        Fill_in 'Caption', with: 'caption'
+        #click submit botton
+        click_boton 'Create Update'
+        #expect the page to have content we submit
+        expect(page).to have_content('title')
+        expect(page).to have_content('caption')
+    end
+end
